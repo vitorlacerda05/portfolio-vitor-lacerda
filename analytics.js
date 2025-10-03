4a9fae14748b2a47570bb05b91cbc3597a38b277
vitorlacerda05 <vitorlacerda05@gmail.com>
2025-10-03 17:08:24 -0300

    Adiciona Google Analytics em todas as pÃ¡ginas com arquivo modular

---
+++ analytics.js
@@ -0,0 +1,9 @@
+// Google Analytics
+// Este arquivo contém o código de rastreamento do Google Analytics
+// ID: G-S8YR8WFL8C
+
+// Configura o Google Analytics
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-S8YR8WFL8C');
